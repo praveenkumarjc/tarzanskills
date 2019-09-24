3be709ee23114bf28fb49d845bce3f55a17c99df
praveenkumarjc <praveenkumarjc323@gmail.com>
2019-09-24 11:15:59 +0530

    comit to branch

--- iobyuser.py
+++ iobyuser.py
@@ -1,3 +1,10 @@
 a=int(input('enter the number A\n'))
 b=int(input('enter the number B\n'))
-print(a+b)
+print('addition',a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+n=int(input("enter n\n"))
+for i in range(n):
+    print(i)
+
